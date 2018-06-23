8187aff928f00ef48f0879ff5693680fdfce4dba
Rifat Bin Masud <rifat.masud@northsouth.edu>
2018-06-23 22:11:42 +0600

    Add files via upload

---
+++ lab1/Calculator.h
@@ -0,0 +1,18 @@
+#ifndef CALCULATOR_H_INCLUDED
+#define CALCULATOR_H_INCLUDED
+
+class Calculator {
+
+private:
+    int  var1,var2, resVar;
+
+public:
+
+    Calculator();
+    ~Calculator();
+    int addition();
+    int show();
+
+};
+
+#endif // CALCULATOR_H_INCLUDED
---
+++ lab1/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "Calculator.h"
+
+using namespace std;
+
+int main()
+{
+  Calculator c1;
+  c1.addition();
+  cout<<c1.show();
+  main();
+}
