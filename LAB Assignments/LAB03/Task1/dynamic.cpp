c0cd9f64e39c469cdb04eff62edcc967df3211e2
Rifat Bin Masud <rifat.masud@northsouth.edu>
2018-06-29 12:01:16 +0600

    Create dynamic.cpp

---
+++ LAB Assignments/LAB03/Task1/dynamic.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include "dynamic.h"
+using namespace std;
+
+template <class ARB>
+dynamic<ARB>::dynamic(int s)
+{
+    data = new ARB[s];
+    size = s;
+}
+template <class ARB>
+dynamic<ARB>::~dynamic()
+{
+    delete [] data;
+}
+
+template <class ARB>
+ARB dynamic<ARB>::getValue(int index)
+{
+ return data[index];
+}
+
+template <class ARB>
+void dynamic<ARB>::setValue(int index, ARB value)
+{
+    data[index] = value;
+}
