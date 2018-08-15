1ec7f4c70f68b607bd19014ce12f204eed7e2bd2
Rifat Bin Masud <rifat.masud@northsouth.edu>
2018-08-15 12:21:48 +0600

    Create hello.py

---
+++ Exception/hello.py
@@ -0,0 +1,29 @@
+#Basic Python DAY1.. This is how to comment
+
+print ("Hello world")   #HW
+
+var = "Hallo"   #gone are the days of type specifying
+
+var2 = 23
+
+var3 = False    #Always capitalize bool
+
+print(not var3)
+
+print (var)
+
+print(var2) #Also no need for semicolon
+
+phrase = "HEY MAMA"
+
+print(phrase.lower())       #lower cases all, string function, tons of these
+
+name_me = input("Whatsername? ")
+
+print("You are that shit "+ name_me)        #input output
+
+num1 = input()
+num2 = input()
+
+print(int(num1)+ int(num2)) #by default python parses input as string, this is sorta typecasting to int
+
