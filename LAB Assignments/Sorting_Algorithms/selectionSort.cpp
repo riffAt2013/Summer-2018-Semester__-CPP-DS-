244d7bf65bc591db8614e75e1aae550638b71cd4
Rifat Bin Masud <rifat.masud@northsouth.edu>
2018-08-29 11:59:08 +0600

    Update selectionSort.cpp

--- LAB Assignments/Sorting_Algorithms/selectionSort.cpp
+++ LAB Assignments/Sorting_Algorithms/selectionSort.cpp
@@ -1,1 +1,36 @@
+#include <iostream>
+using namespace std;
+
+void printSteps (int arr[], int n)
+{
+    for (int i=0; i<n; i++)
+        cout<<arr[i]<<" ";
+}
+
+void selectionSort (int arr[], int n)
+{
+    int i, j;
+
+    for (i = 0; i<n; i++)
+    {
+        for (j = i; j<n; j++)
+        {
+            if (arr[i] > arr[j])
+                swap (arr[i], arr[j]);
+        }
+        printSteps (arr, n);
+                cout<<endl;
+    }
+}
+
+
+
+int main()
+{
+    int arr[] = {14, 33, 27,10,35,19,48,44};
+    int n =sizeof(arr) / sizeof(arr[0]);
+
+    selectionSort (arr, n);
+
+}
 
